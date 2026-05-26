d965c2dacfe326881a4f5206b18722b708c1d850
Ubuntu <kaybeelateef@gmail.com>
2026-05-26 18:26:42 +0200

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,29 @@ resource "aws_instance" "vendor_server" {
     Name = "vendor-onboarding-server"
   }
 }
+
+resource "aws_security_group" "vendor_sg" {
+
+  name = "vendor-security-group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
